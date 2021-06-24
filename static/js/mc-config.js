474dc1144a261f460846302699f905307c2711c0
SMT <83726258+scottmakestech@users.noreply.github.com>
2021-06-24 12:48:15 -0500

    Rework mailchimp config

---
+++ static/js/mc-config.js
@@ -0,0 +1,27 @@
+(function ($) {
+  window.fnames = new Array();
+  window.ftypes = new Array();
+  fnames[0] = "EMAIL";
+  ftypes[0] = "email";
+  fnames[2] = "MMERGE2";
+  ftypes[2] = "dropdown";
+  fnames[1] = "PRIVACYPOL";
+  ftypes[1] = "radio";
+  fnames[3] = "MMERGE3";
+  ftypes[3] = "text";
+  fnames[4] = "MMERGE4";
+  ftypes[4] = "text";
+  fnames[5] = "MMERGE5";
+  ftypes[5] = "text";
+  fnames[6] = "MMERGE6";
+  ftypes[6] = "text";
+  fnames[7] = "MMERGE7";
+  ftypes[7] = "text";
+  fnames[8] = "MMERGE8";
+  ftypes[8] = "text";
+  fnames[9] = "MMERGE9";
+  ftypes[9] = "number";
+  fnames[10] = "MMERGE10";
+  ftypes[10] = "text";
+})(jQuery);
+var $mcj = jQuery.noConflict(true);
