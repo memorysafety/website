181db1d9542dd1bb8d3ab7ed708ad90148cb5391
scottmakestech <83726258+scottmakestech@users.noreply.github.com>
2022-07-19 11:03:19 -0500

    Autoanchors (#23)
    * Adding autoanchors
    
    * Finalize autoanchors with icon, adding .page-content to relevant templates, and scroll offset to prevent sticky nav overlapping the target headings

---
+++ static/js/site.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function(){
+    setupAutoanchors();
+})
+
+function setupAutoanchors() {
+    var page = document.querySelector(".page-content");
+    if (page) {
+        var selector = "h1,h2,h3"; //
+        page.querySelectorAll(selector).forEach(function(el){
+            if (!el.id) {
+                // Create an ID for the element
+                var id = el.innerText.toLowerCase().replace(/\s/g, "-");
+                if (document.getElementById(id)) {
+                    // If the ID already exists, add a number to the end
+                    var i = 1;
+                    while (document.getElementById(id + "-" + i)) {
+                        i++;
+                    }
+                    id += "-" + i;
+                }
+                el.id = id;
+                el.classList.add("scroll-offset-for-sticky-nav");
+            }
+            if (el.id) {
+                var icon = document.createElement("a");
+                icon.className = "autoanchor fas fa-link";
+                icon.href = "#"+el.id;
+                icon.innerHTML = "&nbsp;"
+                el.appendChild(icon);
+            }
+        });
+    }
+}
